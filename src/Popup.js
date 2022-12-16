e5b3f18d02375f2a27f24d1c7cd0ddffaca4c1a9
star0717 <ydb0717@gmail.com>
2022-12-16 21:02:47 +0900

    a

--- src/Popup.js
+++ src/Popup.js
@@ -5,12 +5,22 @@ function Popup() {
   const [popupVisible, setPopupVisivle] = useState(true);
   return (
     <>
-      <button
-        className="btn btn-outline"
-        onClick={() => setPopupVisivle(false)}
-      >
-        close
-      </button>
+      {popupVisible && (
+        <button
+          className="btn btn-outline"
+          onClick={() => setPopupVisivle(false)}
+        >
+          close
+        </button>
+      )}
+      {popupVisible || (
+        <button
+          className="btn btn-outline"
+          onClick={() => setPopupVisivle(true)}
+        >
+          open
+        </button>
+      )}
       <hr />
       {popupVisible && <div style={{ width: 100, height: 100, border }}></div>}
     </>
