32578bbe844791343fbac0adff0ce955c2ef6e1a
star0717 <ydb0717@gmail.com>
2022-12-16 19:35:08 +0900

    a

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -4,18 +4,32 @@ import logo from "./logo.svg";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
-  const noIsEvenDiv = (
-    <>
-      <hr />
-      <span>짝수입니다</span>
-    </>
-  );
+  const noIsEvenDiv =
+    no % 2 == 0 ? (
+      <>
+        <hr />
+        <span>짝수입니다</span>
+      </>
+    ) : (
+      <>
+        <hr />
+        <span>홀수입니다</span>
+      </>
+    );
   return (
     <>
       숫자 : {no}
       <hr />
       <button onClick={() => setNo(no + 1)}>증가</button>
       {noIsEvenDiv}
+      {no % 8 == 0 ? (
+        <>
+          <hr />
+          <span>8의 배수입니다</span>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
